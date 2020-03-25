b010ea47f052b154d453b3cbfabb8ccfe6c20308
Eric Montague <eric.g.montague@gmail.com>
2020-03-25 19:34:07 -0400

    updated scrapy folder name

--- server/scrapy/performances_scraper/middlewares.py
+++ server/scrapy/performances_scraper/middlewares.py
@@ -3,7 +3,7 @@
 # Define here the models for your spider middleware
 #
 # See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# http://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
 
