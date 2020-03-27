a0102713630c3525a9190d69dca7894e00c0fa80
Eric Montague <eric.g.montague@gmail.com>
2020-03-26 22:38:25 -0400

    updated artist model

--- server/app/models/artist.py
+++ server/app/models/artist.py
@@ -9,7 +9,7 @@ class Artist(db.Model):
 
     __tablename__ = "artists"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
+    name = db.Column(db.String(64), unique=True, index=True, nullable=False)
     bio = db.Column(db.Text(), nullable=True)
     website = db.Column(db.Text(), nullable=True)
     performances = db.relationship(
