be8a8d186c0f0dc1e6c17cc25ed1ab95e3ee69a5
Eric Montague <eric.g.montague@gmail.com>
2020-03-28 21:00:27 -0400

    updated how token is gotten during tests

---
+++ server/tests/flask_app/api/images/test_general_access.py
@@ -0,0 +1,53 @@
+"""This module contains tests for general access to the venue endpoints."""
+
+
+from pytest import mark
+from flask_app.utils import get_headers
+
+
+@mark.parametrize(
+    "http_method, endpoint",
+    [
+        ("GET", "/api/v1/images"),
+        ("POST", "/api/v1/images"),
+        ("GET", "/api/v1/images/1"),
+        ("PUT", "/api/v1/images/1"),
+        ("DELETE", "/api/v1/images/1")
+    ]
+)
+def test_unauthorized_access(flask_test_client, http_method, endpoint):
+    """Test to ensure that a 401 HTTP status is returned
+    when the image routes are accessed
+    without a token.
+    """
+    response = flask_test_client.open(
+        method=http_method, path=endpoint
+    )
+    assert response.status == "401 UNAUTHORIZED"
+    assert response.content_type == "application/json"
+    assert response.json["message"] == "Access token is invalid or expired."
+
+
+@mark.parametrize(
+    "http_method, endpoint",
+    [
+        ("PUT", "/api/v1/venues"),
+        ("PATCH", "/api/v1/venues"),
+        ("DELETE", "/api/v1/venues"),
+        ("POST", "/api/v1/venues/1"),
+        ("PATCH", "/api/v1/venues/1")
+    ]
+)
+def test_method_not_allowed(flask_test_client, auth, user, http_method, endpoint):
+    """Test to ensure that a 405 HTTP status is returned
+    when an endpoint doesn't support a particular
+    HTTP method.
+    """
+    token = auth.register(user.username, "password", user.email)
+    response = flask_test_client.open(
+        method=http_method, path=endpoint, headers=get_headers(token)
+    )
+    assert response.status == "405 METHOD NOT ALLOWED"
+    assert response.content_type == "application/json"
+    assert response.json["message"] == "The method is not allowed for the requested URL."
+    
