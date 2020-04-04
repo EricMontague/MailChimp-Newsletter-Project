7300ed0ecb8f30dcb6e91cbc7ec721de1222ef88
Eric Montague <eric.g.montague@gmail.com>
2020-04-03 22:59:29 -0400

    added artist_id and venue_id attributes to performance schema

--- server/app/api/schemas/performance.py
+++ server/app/api/schemas/performance.py
@@ -1,9 +1,10 @@
 """This module contains the performance schema."""
 
 
+from datetime import date
 from app.extensions import ma
 from app.models import Performance
-from marshmallow import post_load, ValidationError, validate, validates_schema
+from marshmallow import post_load, ValidationError, validate, validates_schema, validates
 
 
 class PerformanceSchema(ma.SQLAlchemySchema):
@@ -13,17 +14,22 @@ class PerformanceSchema(ma.SQLAlchemySchema):
         model = Performance
 
     id = ma.auto_field(dump_only=True)
-    title = ma.auto_field(validate=validate.Length(min=1, max=128))
+    title = ma.auto_field(required=True, validate=validate.Length(min=1, max=128))
     description = ma.auto_field(validate=validate.Length(min=1))
     url = ma.Url(required=True)
-    start_datetime = ma.auto_field(required=True) #iso format by default
-    end_datetime = ma.auto_field(required=True) #iso format by default
+    start_datetime = ma.auto_field(required=True, format="%m/%d/%Y %H:%M")
+    end_datetime = ma.auto_field(required=True, format="%m/%d/%Y %H:%M")
+    artist_id = ma.auto_field(required=True, load_only=True)
+    venue_id = ma.auto_field(required=True, load_only=True)
     artist = ma.HyperlinkRelated("api.artist", url_key="artist_id")
     venue = ma.HyperlinkRelated("api.venue", url_key="venue_id")
 
-    _links = ma.Hyperlinks({
-        "uri": ma.URLFor("api.performance", performance_id="<id>"), "collection": ma.URLFor("api.performance_list")
-    })
+    _links = ma.Hyperlinks(
+        {
+            "uri": ma.URLFor("api.performance", performance_id="<id>"),
+            "collection": ma.URLFor("api.performance_list"),
+        }
+    )
 
     @post_load
     def make_object(self, data, **kwargs):
@@ -37,6 +43,10 @@ class PerformanceSchema(ma.SQLAlchemySchema):
         """Raise a ValidationError if the start_datetime is after
         the end_datetime.
         """
-        if data["start_datetime"] > data["end_datetime"]:
-            raise ValidationError("'start_datetime' must be after the 'end_datetime'")
-    
+        #start date is after end date
+        if data["start_datetime"].date() > data["end_datetime"].date():
+            raise ValidationError("Start date must be before end date.")
+        #start date and end date are the same day, but start time is after end time
+        elif data["start_datetime"].date() == data["end_datetime"].date() \
+                and data["start_datetime"].time() > data["end_datetime"].time():
+                raise ValidationError("Start time must be before end time.")
