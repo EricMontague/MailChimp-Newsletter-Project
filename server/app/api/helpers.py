3785d7064c917225d470cf1bf0df0ff63ee18638
Eric Montague <eric.g.montague@gmail.com>
2020-04-03 22:53:09 -0400

    added string to date function

--- server/app/api/helpers.py
+++ server/app/api/helpers.py
@@ -2,17 +2,18 @@
 
 
 from flask import request, url_for
+from datetime import datetime
 
 
 DEFAULT_PAGE_SIZE = 50
 DEFAULT_PAGE_NUMBER = 1
 
 
-def paginate(model, schema):
+def paginate(tablename, query, schema):
     """Return a paginated collection of resources."""
     page = request.args.get("page", DEFAULT_PAGE_NUMBER, type=int)
     per_page = request.args.get("per_page", DEFAULT_PAGE_SIZE, type=int)
-    pagination = model.query.paginate(page=page, per_page=per_page, error_out=False)
+    pagination = query.paginate(page=page, per_page=per_page, error_out=False)
     prev = None
     if pagination.has_prev:
         prev = url_for(request.endpoint, page=page - 1)
@@ -20,8 +21,19 @@ def paginate(model, schema):
     if pagination.has_next:
         next = url_for(request.endpoint, page=page + 1)
     return {
-        model.__tablename__: schema.dumps(pagination.items, many=True),
+        tablename: schema.dumps(pagination.items, many=True),
         "prev": prev,
         "next": next,
         "total": pagination.total
     }
+
+
+def string_to_date(date_string, format):
+    """Given a string date and format, return a 
+    corresponding date object.
+    """
+    try:
+        return datetime.strptime(date_string, format).date()
+    except ValueError:
+        return None
+    
