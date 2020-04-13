4b4b8c59c3678e10d9c2c9919e010d8e89700262
Eric Montague <eric.g.montague@gmail.com>
2020-04-12 23:49:49 -0400

    no changes

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,7 +1,7 @@
-from .user import User
-from .artist import Artist
-from .venue import Venue
-from .image import Image
-from .performance import Performance
+from models.user import User
+from models.artist import Artist
+from models.venue import Venue
+from models.image import Image
+from models.performance import Performance
 
 
