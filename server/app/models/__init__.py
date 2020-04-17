1484cadb5f7742aac37ce8466ff6cfe616e5849c
Eric Montague <eric.g.montague@gmail.com>
2020-04-16 22:02:33 -0400

    changed import statements

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,7 +1,9 @@
-from models.user import User
-from models.artist import Artist
-from models.venue import Venue
-from models.image import Image
-from models.performance import Performance
+"""This package contains models for the application."""
+
+from app.models.user import User
+from app.models.artist import Artist
+from app.models.venue import Venue
+from app.models.image import Image
+from app.models.performance import Performance
 
 
