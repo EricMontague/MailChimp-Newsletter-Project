4c45a9948d303aa2a1b2cecc3f12afe4d897fc04
Eric Montague <eric.g.montague@gmail.com>
2020-04-16 23:27:08 -0400

    added method to make calls to the API

--- server/scrapy/performance_scraper/api_client.py
+++ server/scrapy/performance_scraper/api_client.py
@@ -3,7 +3,6 @@
 
 import requests
 import json
-from performance_scraper.auth import AuthenticationClient
 
 
 class FlaskAPIClient:
@@ -11,9 +10,9 @@ class FlaskAPIClient:
 
     api_prefix = "/api/v1/"
 
-    def __init__(self, token, auth_manager):
+    def __init__(self, token=None, auth_manager=None):
         self._token = token
-        self._auth_manager = auth_manager
+        self.auth_manager = auth_manager
 
     def create_venue(self):
         pass
@@ -59,19 +58,47 @@ class FlaskAPIClient:
         #return nothing
         pass
 
-    def _auth_headers(self):
-        """Return auth headers necessary for making requests
+    def _get_headers(self):
+        """Return the headers necessary for making requests
         to the api.
         """
         if self._token is not None:
             headers = {"Authorization": f"Bearer {self._token}"}
-        elif self._auth_manager:
-            token = self._auth_manager.login()
+        elif self.auth_manager is not None:
+            token = self.auth_manager.login()
             self._token = token
             headers = {"Authorization": f"Bearer {self._token}"}
+        else:
+            headers = {}
+        headers["Content-Type"] = "application/json"
+        headers["Accept"] = "application/json"
         return headers
 
-    def _internal_call(self):
+    def _internal_call(self, method, url, payload=None):
         """Method for making calls to the API."""
-        pass
+        kwargs = {}
+        headers = self._get_headers()
+        if payload is not None:
+            kwargs = json.dumps(payload)
+        try:
+            response = requests.request(
+                method,
+                self.api_prefix + url,
+                headers=headers,
+                **kwargs
+            )
+            response.raise_for_status()
+            json_response = response.json()
+        except requests.exceptions.HTTPError:
+            #attempt to retrieve error message
+            try:
+                message = response.json()["message"]
+            except (ValueError, KeyError):
+                message = "An error occurred."
+            #logging needed here
+            raise FlaskAPIException
+        except ValueError: #a put request will not return a JSON response
+            json_response = None
+        return json_response
+        
 
