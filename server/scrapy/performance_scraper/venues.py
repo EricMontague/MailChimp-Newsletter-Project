75e7b8ad02c33ba04b6e61223a5cc84e7bf53b5e
Eric Montague <eric.g.montague@gmail.com>
2020-04-16 21:57:15 -0400

    created file to store venue items

---
+++ server/scrapy/performance_scraper/venues.py
@@ -0,0 +1,14 @@
+"""This module contains venue items."""
+
+
+from performance_scraper.items import VenueItem
+
+
+#Heritage
+heritage_item = VenueItem()
+heritage_item["name"] = "Heritage"
+heritage_item["street_address"] = "914 N. 2nd St."
+heritage_item["city"] = "Philadelphia"
+heritage_item["state"] = "PA"
+heritage_item["zip_code"] = "19123"
+
