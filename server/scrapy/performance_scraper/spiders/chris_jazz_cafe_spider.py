de86333cb78cffe19ce5e485614e83397aba0e50
Eric Montague <eric.g.montague@gmail.com>
2020-04-23 00:37:41 -0400

    no change

--- server/scrapy/performance_scraper/spiders/chris_jazz_cafe_spider.py
+++ server/scrapy/performance_scraper/spiders/chris_jazz_cafe_spider.py
@@ -8,7 +8,7 @@ from scrapy.linkextractors import LinkExtractor
 class ChrisJazzCafeSpider(CrawlSpider):
     """Spider to crawl Chris' Jazz Cafe for performances."""
 
-    name = "chris_jazz_cafe_spider"
+    name = "chris_jazz_cafe"
     start_urls = ["https://www.chrisjazzcafe.com/calendar"]
 
     def parse(self, response):
