66420b7455088217fb04f039c9ee93307acdc5c5
Eric Montague <eric.g.montague@gmail.com>
2020-04-23 00:32:04 -0400

    refactored put method to handle file uploads

--- server/app/api/resources/image.py
+++ server/app/api/resources/image.py
@@ -5,11 +5,12 @@ import os
 from flask import request, current_app
 from flask_restful import Resource
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 from marshmallow import ValidationError
 from app.models import Image, Artist
 from app.extensions import db
 from http import HTTPStatus
-from app.api.helpers import allowed_file_extension
+from app.api.helpers import allowed_file_extension, create_directory
 
 
 class ArtistImageListAPI(Resource):
@@ -33,12 +34,14 @@ class ArtistImageListAPI(Resource):
         artist = Artist.query.get(artist_id)
         if artist is None:
             return {"message": "Artist could not be found."}, HTTPStatus.NOT_FOUND
-        file = request.files.get("files")
+        file = request.files.get("artist_image")
         if file is None:
             return (
                 {"message": "Could not find an image file in the request."},
                 HTTPStatus.BAD_REQUEST,
             )
+        if file.filename == "":
+            return {"message": "No selected file."}, HTTPStatus.BAD_REQUEST
         if not allowed_file_extension(file.filename):
             return (
                 {
@@ -46,6 +49,7 @@ class ArtistImageListAPI(Resource):
                 },
                 HTTPStatus.BAD_REQUEST,
             )
+        create_directory(current_app.config["UPLOAD_DIRECTORY"])
         #delete the artist's current image from the directory if one exists
         if artist.image is not None:
             try:
@@ -53,10 +57,14 @@ class ArtistImageListAPI(Resource):
             except FileNotFoundError:
                 pass
         filename = secure_filename(file.filename)
-        file.save(current_app.config["UPLOAD_FOLDER"], filename)
-        image = Image(
-            path=current_app.config["UPLOAD_FOLDER"] + "/" + filename
-        )
-        artist.image = image
+        destination = current_app.config["UPLOAD_DIRECTORY"] + "/" + filename
+        file.save(destination)
+        #the below block isn't in the above if statement because I want to separate out the
+        #file system operations from the database operations
+        if artist.image is None:
+            image = Image(path=destination)
+            artist.image = image
+        else:
+            artist.image.path = destination 
         db.session.commit()
         return "", HTTPStatus.NO_CONTENT
