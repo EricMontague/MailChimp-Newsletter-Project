2cb5fe5c50298402dd5b722abe128c1d1ca27ff6
Eric Montague <eric.g.montague@gmail.com>
2020-04-23 00:33:56 -0400

    refactored internal call and udpate_artist_image methods to handle file uploads

--- server/scrapy/performance_scraper/flask_api/api_client.py
+++ server/scrapy/performance_scraper/flask_api/api_client.py
@@ -36,14 +36,13 @@ class FlaskAPIClient:
         """Send a GET request to the API to retrieve an artist by name."""
         return self._internal_call("GET", f"artists/{name}")
 
-    def upload_artist_image(self, artist_id, payload):
+    def upload_artist_image(self, artist_id, image_file):
         """Send a PUT request to the API to replace an artist's image. If an image 
         doesn't exist, then one is created. Returns None.
         """
-        #Scrapy automatically uses Pillow to convert all images to JPEG by default
-        content_type = "image/jpeg"
         return self._internal_call(
-            "PUT", f"artists/{artist_id}/images", payload=payload, content_type=content_type
+            "PUT", f"artists/{artist_id}/images", 
+            payload={"artist_image": image_file}
         )
 
     def create_performance(self, payload):
@@ -67,27 +66,28 @@ class FlaskAPIClient:
         headers["Accept"] = "application/json"
         return headers
 
-    def _internal_call(self, method, url, payload=None, query_string_parameters=None, **kwargs):
+    def _internal_call(self, method, url, payload=None, **kwargs):
         """Method for making calls to the API."""
-        data = {}
+        data = {"json": {}}
         retries = 5
         json_response = None
         headers = self._get_headers()
-        if kwargs.get("content_type"):
-            headers["Content-Type"] = kwargs["content_type"]
+        if payload is not None:
+            if payload.get("artist_image") is not None:
+                headers.pop("Content-Type")
+                data = {"files": payload}
+            else:
+                data = {"json": payload}
         while retries > 0:
-            if payload is not None:
-                data = json.dumps(payload)
             try:
                 response = requests.request(
                     method,
                     self.api_prefix + url,
                     headers=headers,
-                    data=data,
-                    params=query_string_parameters
+                    **data,
                 )
-                response.raise_for_status()
                 json_response = response.json()
+                response.raise_for_status()
                 break
             except requests.exceptions.HTTPError:
                 #token is expired, need to retrieve a new one a try again
