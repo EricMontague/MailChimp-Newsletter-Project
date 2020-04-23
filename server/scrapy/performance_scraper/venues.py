95b261e01f6c6928ddca11e23c773de680f1a605
Eric Montague <eric.g.montague@gmail.com>
2020-04-23 00:36:14 -0400

    added new venues

--- server/scrapy/performance_scraper/venues.py
+++ server/scrapy/performance_scraper/venues.py
@@ -21,3 +21,27 @@ paris_bistro_item["city"] = "Philadelphia"
 paris_bistro_item["state"] = "PA"
 paris_bistro_item["zip_code"] = "19118"
 
+#Time
+time_item = VenueItem()
+time_item["name"] = "Time Restaurant, Whiskey Bar, & Taproom"
+time_item["street_address"] = "1315 Sansom St"
+time_item["city"] = "Philadelphia"
+time_item["state"] = "PA"
+time_item["zip_code"] = "19107"
+
+#Candlelight
+candlelight_item = VenueItem()
+candlelight_item["name"] = "Candlelight Lounge"
+candlelight_item["street_address"] = "24 Passaic St,"
+candlelight_item["city"] = "Trenton"
+candlelight_item["state"] = "NJ"
+candlelight_item["zip_code"] = "08618"
+
+
+#@exuberance
+exuberance_item = VenueItem()
+exuberance_item["name"] = "@exuberance"
+exuberance_item["street_address"] = "1220 N. Mascher St."
+exuberance_item["city"] = "Philadelphia"
+exuberance_item["state"] = "PA"
+exuberance_item["zip_code"] = "19122"
