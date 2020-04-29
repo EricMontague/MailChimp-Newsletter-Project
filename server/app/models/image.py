68e6d0f8c4dc3da193cef89472636ab30f843bb3
Eric Montague <eric.g.montague@gmail.com>
2020-04-29 17:17:30 -0400

    added new columns to image model

--- server/app/models/image.py
+++ server/app/models/image.py
@@ -10,6 +10,8 @@ class Image(db.Model):
     __tablename__ = "images"
     id = db.Column(db.Integer, primary_key=True)
     path = db.Column(db.Text(), unique=True, index=True, nullable=False)
+    original_filename = db.Column(db.Text(), index=True, nullable=True)
+    version = db.Column(db.Integer, default=1, nullable=True)
     artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
 
     def __repr__(self):
