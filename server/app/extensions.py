1565984d38894e94e16f7d52c29f2f0fd7f2cd6f
Eric Montague <eric.g.montague@gmail.com>
2020-05-03 00:06:52 -0400

    added celery to the extensions file

--- server/app/extensions.py
+++ server/app/extensions.py
@@ -5,9 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from celery import Celery
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
 jwt = JWTManager()
+celery = Celery()
+
