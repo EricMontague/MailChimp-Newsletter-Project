2177d69fdc297c4ebed16bc079b1201eaad6f085
Eric Montague <eric.g.montague@gmail.com>
2020-05-03 22:41:45 -0400

    setup skeleton for implementing celery task

--- server/app/scrapy/performance_scraper/tasks.py
+++ server/app/scrapy/performance_scraper/tasks.py
@@ -1,9 +1,22 @@
-from celery import Celery
+"""This module contains celery tasks for running the spiders
+programatically."""
 
 
+from app.celery_app import celery_app
+from scrapy.exceptions import DropItem, CloseSpider
+from scrapy.crawler import CrawlRunner
+from scrapy.utils.log import configure_logging
+from twisted.internet import reactor
 
 
-@app.task(throws=(DropItem, CloseSpider))
-def start_crawl(*args, **kwargs):
+@celery_app.task(bind=True, throws=(DropItem, CloseSpider))
+def start_crawl(spider):
+    """Start a crawl using the given spider's name
+    passed as a string.
+    """
     pass
+
+
+
+
 
