466a4cfb9e2742ae9ffde5318846927840ccc3ce
Eric Montague <eric.g.montague@gmail.com>
2020-05-03 22:42:09 -0400

    added celery configuration variables

--- server/config.py
+++ server/config.py
@@ -11,9 +11,11 @@ class BaseConfig:
     """Base class for application configuration"""
 
     SECRET_KEY = "fake key for development only"
+
     JWT_IDENTITY_CLAIM = "sub"
     JWT_ERROR_MESSAGE_KEY = "message"
     JWT_ACCESS_TOKEN_EXPIRES = 1800 #30 minutes
+
     DB_USER = "ericmontague"
     DB_PASSWORD = "password"
     DB_NAME = "mailchimp"
@@ -21,13 +23,17 @@ class BaseConfig:
     DB_PORT = 5432
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     CSRF_ENABLED = True #check later to see if this is really needed
     DEFAULT_RESOURCES_PER_PAGE = 50
+
     UPLOAD_DIRECTORY = basedir + "/app/static/artist_images"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
     CELERY_RESULTS_EXPIRE = 60 * 60 * 24 * 2, #two days
     CELERY_TIMEZONE = "US/Eastern"
+    CELERY_INCLUDE =  ["app.scrapy.performance_scraper.tasks"] 
 
     @staticmethod
     def init_app(app):
