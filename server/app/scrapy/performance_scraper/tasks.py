b0bba326f192e25fab557e0a9dcac8e4755f3922
Eric Montague <eric.g.montague@gmail.com>
2020-05-04 23:49:15 -0400

    wrote function to start celery task

--- server/app/scrapy/performance_scraper/tasks.py
+++ server/app/scrapy/performance_scraper/tasks.py
@@ -3,10 +3,14 @@ programatically."""
 
 
 from app.celery_app import celery_app
+from scrapy.spiderloader import SpiderLoader
 from scrapy.exceptions import DropItem, CloseSpider
-from scrapy.crawler import CrawlRunner
-from scrapy.utils.log import configure_logging
-from twisted.internet import reactor
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+SETTINGS = get_project_settings()
+SPIDERS = SpiderLoader(SETTINGS).list()
 
 
 @celery_app.task(bind=True, throws=(DropItem, CloseSpider))
@@ -14,7 +18,15 @@ def start_crawl(spider):
     """Start a crawl using the given spider's name
     passed as a string.
     """
-    pass
+    process = CrawlerProcess(SETTINGS)
+    process.crawl(spider)
+    process.start()
+    self.update_state(
+        state="PROGRESS",
+        meta={"spider_name": spider}
+    )
+
+
 
 
 
