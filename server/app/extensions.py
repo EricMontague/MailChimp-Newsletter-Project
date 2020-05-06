2733df4795dc0aaa2506ad91f1be44614543ba73
Eric Montague <eric.g.montague@gmail.com>
2020-05-05 21:27:14 -0400

    removed celery

--- server/app/extensions.py
+++ server/app/extensions.py
@@ -5,12 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from celery import Celery
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
 jwt = JWTManager()
-celery = Celery()
+
 
