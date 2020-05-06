af652b294704f0c5e53b827b2c5aaf710c6f8bb7
Eric Montague <eric.g.montague@gmail.com>
2020-05-05 21:28:55 -0400

    moved the import of celery_app after settings to prevent a circular import

--- server/app/scrapy/performance_scraper/tasks.py
+++ server/app/scrapy/performance_scraper/tasks.py
@@ -2,7 +2,7 @@
 programatically."""
 
 
-from app.celery_app import celery_app
+import traceback
 from scrapy.spiderloader import SpiderLoader
 from scrapy.exceptions import DropItem, CloseSpider
 from scrapy.crawler import CrawlerProcess
@@ -11,24 +11,20 @@ from scrapy.utils.project import get_project_settings
 
 SETTINGS = get_project_settings()
 SPIDERS = SpiderLoader(SETTINGS).list()
+from app.celery_app import celery_app #imported here to prevent a circular import
 
 
 @celery_app.task(bind=True, throws=(DropItem, CloseSpider))
-def start_crawl(spider):
+def start_crawl(self, spider):
     """Start a crawl using the given spider's name
     passed as a string.
     """
-    process = CrawlerProcess(SETTINGS)
-    process.crawl(spider)
-    process.start()
     self.update_state(
         state="PROGRESS",
         meta={"spider_name": spider}
     )
-
-
-
-
-
-
+    process = CrawlerProcess(SETTINGS)
+    process.crawl(spider)
+    process.start()
+    
 
