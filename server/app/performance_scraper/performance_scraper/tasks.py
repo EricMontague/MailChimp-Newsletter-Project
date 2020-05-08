99b9d958e8666d29cfd2d68cb24c633ca5cb79b9
Eric Montague <eric.g.montague@gmail.com>
2020-05-08 00:03:28 -0400

    added time.sleep() to task

--- server/app/performance_scraper/performance_scraper/tasks.py
+++ server/app/performance_scraper/performance_scraper/tasks.py
@@ -3,10 +3,12 @@ programatically."""
 
 
 import traceback
+import time
 from scrapy.exceptions import DropItem, CloseSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from app.celery_app import celery_app 
+from celery.exceptions import Ignore
 
 
 SETTINGS = get_project_settings()
@@ -21,10 +23,13 @@ def start_crawl(self, spider):
         state="PROGRESS",
         meta={"spider_name": spider}
     )
+    #need a delay to allow the api to be able to read the task state and
+    #spider name before the task is finished
+    time.sleep(60)
     process = CrawlerProcess(SETTINGS)
     process.crawl(spider)
     process.start()
-    
+    raise Ignore()
 
 
 @celery_app.task(throws=(DropItem, CloseSpider))
