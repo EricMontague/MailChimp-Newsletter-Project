143f6853acb44f3bc81ba6e44cf17e3c8df95bfa
Eric Montague <eric.g.montague@gmail.com>
2020-05-10 22:53:07 -0400

    created list of spider names

---
+++ server/app/performance_scraper/performance_scraper/spiders/__init__.py
@@ -0,0 +1,15 @@
+"""This package contains all of the Spiders for the project."""
+
+
+# hard coded until I can find a way to dynamically retrieve spider names
+SPIDERS = [
+    'candlelight', 
+    'chris_jazz_cafe', 
+    'exuberance', 
+    'heritage', 
+    'kimmel_center',
+    'paris_bistro', 
+    'art_museum', 
+    'time'
+]
+
