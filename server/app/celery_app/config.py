2549249dac87f5f9096d139a739e8240b6858dfb
Eric Montague <eric.g.montague@gmail.com>
2020-05-11 23:26:23 -0400

    reset Celery's backend_results-expiry back to its default time of 1 day

--- server/app/celery_app/config.py
+++ server/app/celery_app/config.py
@@ -11,7 +11,6 @@ class CeleryConfig:
 
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
     CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
-    CELERY_RESULTS_EXPIRE = 60 * 60 * 24 * 2, #two days
     CELERY_TIMEZONE = "US/Eastern"
     CELERY_INCLUDE =  ["app.performance_scraper.performance_scraper.tasks"]
     CELERYBEAT_SCHEDULE = {
