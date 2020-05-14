f294123fab5426dbcfa9c4f1ece6351595f4f5cd
Eric Montague <eric.g.montague@gmail.com>
2020-05-14 00:00:55 -0400

    no changes were made

--- server/app/celery_app/config.py
+++ server/app/celery_app/config.py
@@ -10,7 +10,7 @@ class CeleryConfig:
     """Class that holds configurations for the Celery instance."""
 
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
     CELERY_TIMEZONE = "US/Eastern"
     CELERY_INCLUDE =  ["app.performance_scraper.performance_scraper.tasks"]
     CELERYBEAT_SCHEDULE = {
