38c1223c8588c6ee00e1b77ada6d2fd19d75f534
Eric Montague <eric.g.montague@gmail.com>
2020-05-13 20:05:56 -0400

    moved paginate function into a new module

--- server/app/api/helpers.py
+++ server/app/api/helpers.py
@@ -2,29 +2,8 @@
 
 
 import os
-from flask import request, url_for, current_app
+from flask import current_app
 from datetime import datetime
-
-
-def paginate(tablename, query, schema):
-    """Return a paginated collection of resources."""
-    page = request.args.get("page", default=1, type=int)
-    per_page = request.args.get(
-        "per_page", current_app.config["DEFAULT_RESOURCES_PER_PAGE"], type=int
-    )
-    pagination = query.paginate(page=page, per_page=per_page, error_out=False)
-    prev = None
-    if pagination.has_prev:
-        prev = url_for(request.endpoint, page=page - 1)
-    next = None
-    if pagination.has_next:
-        next = url_for(request.endpoint, page=page + 1)
-    return {
-        tablename: schema.dumps(pagination.items, many=True),
-        "prev": prev,
-        "next": next,
-        "total": pagination.total
-    }
 
 
 def string_to_date(date_string, format):
